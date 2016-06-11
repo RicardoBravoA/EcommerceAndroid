49f290dce596a4aca90804180a41b7b71c17549c
Carlos Vargas <carlos.vargas@orbis.com.pe>
2016-06-10 22:02:12 -0500

    wip(Presentation): se agrego presentation

---
+++ app/src/main/java/com/rba/ecommerce/activity/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.rba.ecommerce.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rba.ecommerce.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, PresentationActivity.class));
+            }
+        }, 1500);
+    }
+}
