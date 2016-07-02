fb3531402fddd9829bd59c6fb90220aaacb3b0da
Ricardo Bravo <ricardobravo@outlook.com>
2016-07-02 12:57:14 -0500

    wip(tabLayout): add fragment for brands

--- app/src/main/java/com/rba/ecommerce/fragment/HomeFragment.java
+++ app/src/main/java/com/rba/ecommerce/fragment/HomeFragment.java
@@ -3,18 +3,29 @@ package com.rba.ecommerce.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.rba.ecommerce.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Ricardo Bravo on 29/05/16.
  */
 
 public class HomeFragment extends Fragment {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     public HomeFragment(){}
 
@@ -26,10 +37,18 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-
-        return rootView;
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        viewPager = (ViewPager) view.findViewById(R.id.viewPager);
+
+        tabLayout = (TabLayout) view.findViewById(R.id.tabLayout);
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
 
     @Override
     public void onAttach(Context context) {
@@ -40,4 +59,44 @@ public class HomeFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
     }
+
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getFragmentManager());
+        adapter.addFragment(new TabFragment().newInstance("uno"), "Uno");
+        adapter.addFragment(new TabFragment().newInstance("dos"), "Dos");
+        adapter.addFragment(new TabFragment().newInstance("tres"), "Tres");
+        viewPager.setOffscreenPageLimit(3);
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentStatePagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
 }
